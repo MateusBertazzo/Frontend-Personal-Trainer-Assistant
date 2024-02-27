bef22557304f90cdaebb012c217254854cc50395
Mateus Bertazzo <mateusbataglin52@gmail.com>
2024-02-27 10:55:22 -0300

    implementa logica de redefinição de senha

--- app/resetPassword/page.tsx
+++ app/resetPassword/page.tsx
@@ -2,9 +2,11 @@
 
 import Button from '../../components/Button';
 import { useForm } from 'react-hook-form';
-import { z } from 'zod';
+import { string, z } from 'zod';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { useState } from 'react';
+import { useRouter, usePathname, useSearchParams } from 'next/navigation';
+
 
 const schema = z.object({
     password: z
@@ -18,40 +20,78 @@ const schema = z.object({
     path: ["confirmPassword"],
 });
   
-  // Tipagem do formulário
+// Tipagem do formulário
 type FormProps = z.infer<typeof schema>;
 
+// Interface para decodificar o token
+interface DecodedToken {
+    email: string | undefined;
+    code: string | undefined;
+}
+
+// Interface para tipar o retorno da requisição
+interface responseData {
+    success: boolean;
+    message: string;
+    response: string | null;
+}
 
 export default function ResetPassword() {
+    // States
+    const [loading, setLoading] = useState(false);
+    const [responseData, setResponseData] = useState<{success: boolean, message: string} | null>(null);
 
+    // Hooks
+    const searchParams = useSearchParams();
+
+    const token = searchParams.get('param') as string;
+
+    const tokenString = atob(token)
+
+    const tokenDecoded: DecodedToken = JSON.parse(tokenString);
+
+    const router = useRouter();
+    const path = usePathname();
     const {
         handleSubmit,
         register,
         formState: { errors },
-    } = useForm<FormProps>({
-        
+    } = useForm<FormProps>({    
         mode: 'onBlur',
         resolver: zodResolver(schema),
     });
 
     const handleForm = async (data : FormProps) => {
         try {
+            setLoading(true);
             const response = await fetch('http://localhost:8080/email/reset-password', {
                 method: 'POST',
                 headers: {
                 'Content-Type': 'application/json',
                 },
-                body: JSON.stringify(data),
+                body: JSON.stringify({
+                    email: tokenDecoded.email, 
+                    code: tokenDecoded.code, 
+                    newPassword: data.password, 
+                    confirmPassword: data.confirmPassword
+                }),
             })
-             
+
+            const responseData = await response.json();
+            setResponseData(responseData);
+            
             if (!response.ok) {
-            console.log('Erro ao redefinir senha');
-            throw new Error('Erro ao redefinir senha');
+                setLoading(false);
+                throw new Error('Erro ao redefinir senha');
             }
 
-            console.log('Senha redefinida com sucesso!');
+            router.push('/');
+
+            if (path === '/') {
+                setLoading(false);
+            }
+            
         } catch (error) {
-            console.error('Erro ao redefinir senha', error);
             throw new Error('Erro ao redefinir senha');
         }
         
@@ -70,6 +110,7 @@ export default function ResetPassword() {
                         placeholder="Digite sua senha"
                         className={`flex flex-col h-12 rounded-md p-2 bg-gray-100 border border-gray-300`}
                     />
+                     {errors.password && <span className='p-1 text-xs text-red-600'>{errors.password.message}</span>}
                 </div>
 
                 <div className='flex flex-col'>
@@ -79,9 +120,11 @@ export default function ResetPassword() {
                         placeholder="Confirme sua senha"
                         className={`flex flex-col h-12 rounded-md p-2 bg-gray-100 border border-gray-300`}
                     />
+                    <p className='self-center'>{responseData?.success && <span className='p-1 text-sm font-bold text-green-600'>{responseData.message}</span>}</p>
+                    {errors.confirmPassword && <span className='p-1 text-xs text-red-600'>{errors.confirmPassword.message}</span>}
                 </div>
 
-                <Button text='Cadastra-se' style='bg-[var(--orange)]' type='submit' />
+                <Button text='Cadastra-se' loading={loading} style='bg-[var(--orange)]' type='submit' />
             </form>
         </div>
     );
